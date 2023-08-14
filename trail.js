b77af40d675d19399ad51155ed6157416123e35f
Ilhamddy <139438329+Ilhamddy@users.noreply.github.com>
2023-08-14 22:35:51 +0800

    Add files via upload

---
+++ trail.js
@@ -0,0 +1,38 @@
+
+function allMatch(man, women){
+
+    console.log("All point match " + callName);
+
+    return(man, women);
+}
+
+
+function callName(mathName){
+
+    var mathName = Math.random() * 100;
+    mathName = Math.floor(mathName) + 1 ;
+
+    return mathName;
+}
+
+var a = prompt("input");
+
+console.log("match " + a + b);
+
+
+
+
+function allMatch(match){
+    var a = prompt("man");
+    var b = prompt("women");
+    var match = Math.random() * 100;
+    match = Math.floor(match) + 1;
+    console.log("All point match "  + a +" and "+ b + " " +match + "%");
+
+    return(match);
+}
+
+console.log();
+
+allMatch();
+
